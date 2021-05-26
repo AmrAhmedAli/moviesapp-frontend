369f8f8043f9c81ac93188c8d6cae1bd32a8368a
AmrAhmedAli <amr.ali-hassan@student.guc.edu.eg>
2021-05-26 05:41:06 +0200

    fixed mobile header

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { AuthCheckState, AuthLogout } from "./actions/AuthActions";
 import { Dropdown } from "react-bootstrap";
 import Signup from "./containers/Signup";
 import { Add } from "@material-ui/icons";
+import { Menu } from "@material-ui/icons";
 function App() {
   const [className, setClassName] = useState("AppBar");
   const dispatch = useDispatch();
@@ -37,55 +38,131 @@ function App() {
     <div className="App">
       <AppBar position="fixed" className={className}>
         <Toolbar className="toolbar-container">
-          <div className="left-toolbar-container">
-            <img className="logo" src={process.env.PUBLIC_URL + "/logo.png"} />
-            <nav className="intial-nav">
-              <NavLink to={"/"}>Home</NavLink>
+          <div className="desktop-view">
+            <div className="left-toolbar-container">
+              <img
+                className="logo"
+                src={process.env.PUBLIC_URL + "/logo.png"}
+              />
+              <nav className="intial-nav">
+                <NavLink to={"/"}>Home</NavLink>
 
-              {localStorage.getItem("token") && (
-                <NavLink to={"/my-movies"}>My Movies</NavLink>
-              )}
-            </nav>
+                {localStorage.getItem("token") && (
+                  <NavLink to={"/my-movies"}>My Movies</NavLink>
+                )}
+              </nav>
+            </div>
+            <div className="right-toolbar-container">
+              <nav>
+                {localStorage.getItem("token") && (
+                  <div className="right-flex">
+                    <IconButton
+                      aria-label=""
+                      size="small"
+                      className="add-movie"
+                      onClick={executeScroll}
+                    >
+                      <Add fontSize="inherit" />
+                      <span className="addnew-tooltiptext">Add movie</span>
+                    </IconButton>
+                    <Dropdown>
+                      <Dropdown.Toggle
+                        className="drop-down"
+                        id="dropdown-basic"
+                      >
+                        Welcome, {localStorage.getItem("username")}
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item className="dropdown-item">
+                          Account
+                        </Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item">
+                          Help
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                          className="dropdown-item"
+                          onClick={handleSignout}
+                        >
+                          Signout
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                )}
+                {!localStorage.getItem("token") && (
+                  <NavLink to={"/login"}>Login</NavLink>
+                )}
+              </nav>
+            </div>
           </div>
-          <div className="right-toolbar-container">
-            <nav>
-              {localStorage.getItem("token") && (
-                <div className="right-flex">
+          <div className="mobile-view">
+            <div className="left-toolbar-container">
+              <img
+                className="logo"
+                src={process.env.PUBLIC_URL + "/logo.png"}
+              />
+            </div>
+            <div className="right-toolbar-container">
+              <Dropdown>
+                <Dropdown.Toggle className="mobile-drop-down">
                   <IconButton
                     aria-label=""
+                    className="burger-menu"
                     size="small"
-                    className="add-movie"
-                    onClick={executeScroll}
                   >
-                    <Add fontSize="inherit" />
-                    <span className="addnew-tooltiptext">Add movie</span>
+                    <Menu fontSize="inherit" />
                   </IconButton>
-                  <Dropdown>
-                    <Dropdown.Toggle className="drop-down" id="dropdown-basic">
-                      Welcome, {localStorage.getItem("username")}
-                    </Dropdown.Toggle>
+                </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                      <Dropdown.Item className="dropdown-item">
-                        Account
-                      </Dropdown.Item>
-                      <Dropdown.Item className="dropdown-item">
-                        Help
-                      </Dropdown.Item>
-                      <Dropdown.Item
-                        className="dropdown-item"
-                        onClick={handleSignout}
-                      >
-                        Signout
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </div>
-              )}
-              {!localStorage.getItem("token") && (
-                <NavLink to={"/login"}>Login</NavLink>
-              )}
-            </nav>
+                <Dropdown.Menu>
+                  {localStorage.getItem("token") && (
+                    <Dropdown.Item className="dropdown-item">
+                      Welcome, {localStorage.getItem("username")}
+                    </Dropdown.Item>
+                  )}
+                  <Dropdown.Item className="dropdown-item" href="/">
+                    Home
+                  </Dropdown.Item>
+                  {localStorage.getItem("token") && (
+                    <Dropdown.Item className="dropdown-item" href="/my-movies">
+                      My Movies
+                    </Dropdown.Item>
+                  )}
+                  {localStorage.getItem("token") && (
+                    <Dropdown.Item
+                      className="dropdown-item"
+                      href="/#add-new-movie"
+                    >
+                      Add movie
+                    </Dropdown.Item>
+                  )}
+                  {!localStorage.getItem("token") && (
+                    <Dropdown.Item className="dropdown-item" href="/login">
+                      Login
+                    </Dropdown.Item>
+                  )}
+                  {localStorage.getItem("token") && (
+                    <Dropdown.Item className="dropdown-item">
+                      Account
+                    </Dropdown.Item>
+                  )}
+                  {localStorage.getItem("token") && (
+                    <Dropdown.Item className="dropdown-item">
+                      Help
+                    </Dropdown.Item>
+                  )}
+                  {localStorage.getItem("token") && (
+                    <Dropdown.Item
+                      className="dropdown-item"
+                      onClick={handleSignout}
+                    >
+                      Signout
+                    </Dropdown.Item>
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
           </div>
         </Toolbar>
       </AppBar>
